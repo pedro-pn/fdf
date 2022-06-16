a4738f4d9205314de3e232acee0454f581fb035d
Pedro Paulo <pe.pn03@gmail.com>
2022-06-16 20:45:09 -0300

    Add events fileand keys header

---
+++ srcs/events.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   events.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 18:00:15 by ppaulo-d          #+#    #+#             */
+/*   Updated: 2022/06/16 20:24:29 by ppaulo-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+#include "keys.h"
+
+int	exit_win(int key, void *param)
+{
+	if (key == ESC)
+	{
+		mlx_clear_window(((t_win_data *)param)->mlx, ((t_win_data *)param)->mlx_win);
+		mlx_destroy_window(((t_win_data *)param)->mlx, ((t_win_data *)param)->mlx_win);
+		mlx_loop_end(((t_win_data *)param)->mlx);
+	}
+	return (0);
+}
