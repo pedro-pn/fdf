3011211675bd81fdd7ab32c04bd09f91ea42f1b8
Pedro Paulo <pe.pn03@gmail.com>
2022-07-02 14:57:54 -0300

    Testing mouse features

--- srcs/main.c
+++ srcs/main.c
@@ -6,15 +6,18 @@
 /*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 10:46:13 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2022/06/29 14:37:10 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2022/07/02 14:43:03 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+int	print_oi(int button, int key, int key2, void *param);
+
 int	main(int argc, char *argv[])
 {
 	t_win_data	mlx_data;
+	int a = 5;
 
 	check_args(argc, argv);
 	fdf_init(&mlx_data.fdf, argv[1]);
@@ -23,7 +26,15 @@ int	main(int argc, char *argv[])
 	mlx_put_image_to_window(mlx_data.mlx, mlx_data.mlx_win,
 		mlx_data.img.img, MENU_WIDTH, 0);
 	mlx_hook(mlx_data.mlx_win, 17, 0, &exit_win, &mlx_data);
+	mlx_hook(mlx_data.mlx_win, 6, 1L<<6, &print_oi, &a);
 	mlx_key_hook(mlx_data.mlx_win, &key_handle, &mlx_data);
 	mlx_loop(mlx_data.mlx);
 	clean_program(&mlx_data, &mlx_data.fdf);
+}
+
+int	print_oi(int button, int key, int key2, void *param)
+{
+	ft_printf("ai");
+	ft_printf("%d %d %d %d\n", button, key, key2, *((int *)param));
+	return 0;
 }
